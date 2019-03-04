dff3777146653783516e6c991790281289f35b8b
Daniel Nedrow <nedrow.2@wright.edu>
2019-03-04 09:27:30 -0500

    Create preliminary FlashCard class, close #1

---
+++ src/flashcards/model/FlashCard.java
@@ -0,0 +1,70 @@
+package flashcards.model;
+
+/**
+ *
+ * @author Daniel
+ */
+public class FlashCard {
+    private String question, answer;
+    private int[] rightWrongHistory;
+    int numAttempts;
+    
+    public FlashCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+        rightWrongHistory = new int[100];
+        numAttempts = 0;
+    }
+    
+    public String getQuestion() {
+        return question;
+    }
+    
+    public String getAnswer() {
+        return answer;
+    }
+    
+    public void setQuestion(String newQuestion) {
+        question = newQuestion;
+    }
+    
+    public void setAnswer(String newAnswer) {
+        answer = newAnswer;
+    }
+    
+    public boolean autoCheckCorrect(String attempt) {
+        if (question.equalsIgnoreCase(attempt)){
+            rightWrongHistory[numAttempts++] = 1;
+            return true;
+        } else {
+            rightWrongHistory[numAttempts++] = 0;
+            return false;
+        }
+    }
+    
+    public void userChecksAnswer(boolean correct) {
+        if (correct) {
+            rightWrongHistory[numAttempts++] = 1;
+        } else {
+            rightWrongHistory[numAttempts++] = 0;
+        }
+    }
+    
+    public double getPercentCorrect() {
+        double sum = 0;
+        for(int i = 0; i < numAttempts; i++) {
+            sum += rightWrongHistory[i];
+        }
+        
+        return sum / numAttempts;
+    }
+    
+    public double getPercentCorrectLast5() {
+        double sum = 0;
+        for (int i = numAttempts - 5; i < numAttempts; i++) {
+            sum += rightWrongHistory[i];
+        }
+        
+        return sum / 5;
+    }
+}
